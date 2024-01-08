bbee309309f6995ec688102c75b734d0459288a7
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-01-08 19:40:21 +0100

    Door bell ring implemented

--- IntercomListenerEsp32/src/main.cpp
+++ IntercomListenerEsp32/src/main.cpp
@@ -27,6 +27,10 @@ int64_t ring_sensor_timestamp = -1;
 int64_t ring_notification_timestamp = -1;
 bool ring_notification_pending = false;
 
+int64_t door_sensor_timestamp = -1;
+int64_t door_notification_timestamp = -1;
+bool door_notification_pending = false;
+
 #if CONFIG_INTERCOM_BOOT_NOTIFICATION
 bool boot_notification_pending = false;
 #endif
@@ -59,7 +63,14 @@ void enter_deep_sleep()
     init_ulp();
 #endif
 
+    
+#if CONFIG_INTERCOM_WAKE_LEVEL == 0
     esp_sleep_enable_ext0_wakeup(static_cast<gpio_num_t>(CONFIG_INTERCOM_RING_GPIO_PIN), CONFIG_INTERCOM_WAKE_LEVEL);
+    esp_sleep_enable_ext1_wakeup(1 << CONFIG_INTERCOM_DOOR_GPIO_PIN, esp_sleep_ext1_wakeup_mode_t::ESP_EXT1_WAKEUP_ALL_LOW);
+#else
+    uin64_t mask = (1 << CONFIG_INTERCOM_RING_GPIO_PIN) | (1 << INTERCOM_DOOR_GPIO_PIN);
+    esp_sleep_enable_ext1_wakeup(mask, esp_sleep_ext1_wakeup_mode_t::ESP_EXT1_WAKEUP_ANY_HIGH);
+#endif
 
 #ifdef CONFIG_INTERCOM_DEEP_SLEEP_DURATION_ENABLED
     ESP_LOGI(main_log_tag, "Waking up in %d seconds...", CONFIG_INTERCOM_DEEP_SLEEP_DURATION);
@@ -72,15 +83,28 @@ void enter_deep_sleep()
 
 void ring_isr_handler(void *arg)
 {
-    uint32_t event_bits;
-    int current_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_RING_GPIO_PIN));
-    if(current_level == CONFIG_INTERCOM_WAKE_LEVEL)
+    auto trigger_gpio_int = reinterpret_cast<uint32_t>(arg);
+    gpio_num_t trigger_gpio = static_cast<gpio_num_t>(trigger_gpio_int);
+
+    uint32_t event_bits = 0;
+    int ring_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_RING_GPIO_PIN));
+    if(ring_level == CONFIG_INTERCOM_WAKE_LEVEL)
     {
-        event_bits = EVENT_RING_SENSOR_START;
+        event_bits |= EVENT_RING_SENSOR_START;
     }
     else
     {
-        event_bits = EVENT_RING_SENSOR_END;
+        event_bits |= EVENT_RING_SENSOR_END;
+    }
+
+    int door_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_DOOR_GPIO_PIN));
+    if(door_level == CONFIG_INTERCOM_WAKE_LEVEL)
+    {
+        event_bits |= EVENT_DOOR_SENSOR_START;
+    }
+    else
+    {
+        event_bits |= EVENT_DOOR_SENSOR_END;
     }
     
     int higherPriorityTaskWoken = false;
@@ -94,22 +118,42 @@ void ring_isr_handler(void *arg)
 void setup_ring_sensor()
 {
     gpio_num_t ring_in = static_cast<gpio_num_t>(CONFIG_INTERCOM_RING_GPIO_PIN);
+    gpio_num_t door_in = static_cast<gpio_num_t>(CONFIG_INTERCOM_DOOR_GPIO_PIN);
     ESP_ERROR_CHECK(rtc_gpio_deinit(ring_in));
+    ESP_ERROR_CHECK(rtc_gpio_deinit(door_in));
     ESP_ERROR_CHECK(gpio_set_direction(ring_in, gpio_mode_t::GPIO_MODE_INPUT));
+    ESP_ERROR_CHECK(gpio_set_direction(door_in, gpio_mode_t::GPIO_MODE_INPUT));
+
 #if CONFIG_INTERCOM_RING_GPIO_PIN_PULL_DOWN
     ESP_ERROR_CHECK(gpio_set_pull_mode(ring_in, GPIO_PULLDOWN_ONLY));
 #elif CONFIG_INTERCOM_RING_GPIO_PIN_PULL_UP
     ESP_ERROR_CHECK(gpio_set_pull_mode(ring_in, GPIO_PULLUP_ONLY));
 #endif 
 
-    int current_level = gpio_get_level(ring_in);
+#if CONFIG_INTERCOM_DOOR_GPIO_PIN_PULL_DOWN
+    ESP_ERROR_CHECK(gpio_set_pull_mode(door_in, GPIO_PULLDOWN_ONLY));
+#elif CONFIG_INTERCOM_RING_GPIO_PIN_PULL_UP
+    ESP_ERROR_CHECK(gpio_set_pull_mode(door_in, GPIO_PULLUP_ONLY));
+#endif 
+
+    int ring_level = gpio_get_level(ring_in);
     ESP_LOGI(main_log_tag, "Setting ring sensor pin to GPIO %d", CONFIG_INTERCOM_RING_GPIO_PIN);
-    ESP_LOGD(main_log_tag, "Current level of GPIO %d: %d", CONFIG_INTERCOM_RING_GPIO_PIN, current_level);
+    ESP_LOGD(main_log_tag, "Current level of Ring GPIO %d: %d", CONFIG_INTERCOM_RING_GPIO_PIN, ring_level);
+
+    int door_level = gpio_get_level(door_in);
+    ESP_LOGI(main_log_tag, "Setting door bell sensor pin to GPIO %d", CONFIG_INTERCOM_DOOR_GPIO_PIN);
+    ESP_LOGD(main_log_tag, "Current level of Door GPIO %d: %d", CONFIG_INTERCOM_DOOR_GPIO_PIN, door_level);
+
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
 
     ESP_ERROR_CHECK(gpio_set_intr_type(ring_in, gpio_int_type_t::GPIO_INTR_ANYEDGE));
     ESP_ERROR_CHECK(gpio_intr_enable(ring_in));
-    ESP_ERROR_CHECK(gpio_install_isr_service(0));
-    ESP_ERROR_CHECK(gpio_isr_handler_add(ring_in, ring_isr_handler, NULL));
+    ESP_ERROR_CHECK(gpio_set_intr_type(door_in, gpio_int_type_t::GPIO_INTR_ANYEDGE));
+    ESP_ERROR_CHECK(gpio_intr_enable(door_in));
+
+    
+    ESP_ERROR_CHECK(gpio_isr_handler_add(ring_in, ring_isr_handler, (void*)ring_in));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(door_in, ring_isr_handler, (void*)door_in));
 }
 
 void send_ring_notification()
@@ -117,13 +161,25 @@ void send_ring_notification()
     ESP_LOGD(main_log_tag, "send_ring_notification called");
 
 #ifdef CONFIG_INTERCOM_TELEGRAM_ENABLED
-    int status_code = telegram_send_notification("Door ring!");
+    int status_code = telegram_send_notification("Intercom Ring!");
     if(status_code != 200)
     {
         led_indicator.set_code(led_indicator_code::http_error);
     }
 #endif
+}
 
+void send_door_notification()
+{
+    ESP_LOGD(main_log_tag, "send_door_notification called");
+
+#ifdef CONFIG_INTERCOM_TELEGRAM_ENABLED
+    int status_code = telegram_send_notification("Door Bell Ring!");
+    if(status_code != 200)
+    {
+        led_indicator.set_code(led_indicator_code::http_error);
+    }
+#endif
 }
 
 #if CONFIG_INTERCOM_BOOT_NOTIFICATION
@@ -175,20 +231,50 @@ extern "C" void app_main()
         ring_notification_pending = true;
         ESP_LOGD(main_log_tag, "ring_notification_pending set to true");
     }
+    else if(wakeup_reason == ESP_SLEEP_WAKEUP_EXT1)
+    {
+        int ring_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_RING_GPIO_PIN));
+        int door_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_DOOR_GPIO_PIN));
+
+        if(ring_level == CONFIG_INTERCOM_WAKE_LEVEL)
+        {
+            ring_sensor_timestamp = esp_timer_get_time();
+            ring_notification_pending = true;
+        }
+
+        if(door_level == CONFIG_INTERCOM_WAKE_LEVEL)
+        {
+            door_sensor_timestamp = esp_timer_get_time();
+            door_notification_pending = true;
+        }
+    }
     else if(wakeup_reason == ESP_SLEEP_WAKEUP_TIMER)
     {
         ESP_LOGI(main_log_tag, "Wake up by TIMER");
         int ring_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_RING_GPIO_PIN));
-        if(ring_level != CONFIG_INTERCOM_WAKE_LEVEL)
-        {
-            timer_alarm_time = CONFIG_INTERCOM_DEEP_SLEEP_DELAY_SHORT;
-            wifi_should_connect = false;
-        }
-        else
+        int door_level = gpio_get_level(static_cast<gpio_num_t>(CONFIG_INTERCOM_DOOR_GPIO_PIN));
+        
+        bool sensed = false;
+        if(ring_level == CONFIG_INTERCOM_WAKE_LEVEL)
         {
             ring_sensor_timestamp = esp_timer_get_time();
             ring_notification_pending = true;
+            sensed = true;
             ESP_LOGD(main_log_tag, "ring_notification_pending set to true");
+        }
+
+        if(door_level == CONFIG_INTERCOM_WAKE_LEVEL)
+        {
+            door_sensor_timestamp = esp_timer_get_time();
+            door_notification_pending = true;
+            sensed = true;
+            ESP_LOGD(main_log_tag, "door_notification_pending set to true");
+        }
+
+        if(!sensed)
+        {
+            timer_alarm_time = CONFIG_INTERCOM_DEEP_SLEEP_DELAY_SHORT;
+            wifi_should_connect = false;
         }
     }
     else
@@ -257,9 +343,37 @@ extern "C" void app_main()
             }
         }
 
+        if((event_bits & EVENT_DOOR_SENSOR_START) == EVENT_DOOR_SENSOR_START)
+        {
+            ESP_LOGD(main_log_tag, "Door start detected!");
+            if(!wifi_should_connect)
+            {
+                wifi_should_connect = true;
+                bool wifi_ok = wifi_init_sta(main_event_group);
+                if(!wifi_ok)
+                {
+                    led_indicator.set_code(led_indicator_code::wifi_error);
+                }
+            }
+
+            int64_t timestamp = esp_timer_get_time();
+            if(door_sensor_timestamp == -1 || (timestamp - door_sensor_timestamp > CONFIG_INTERCOM_RING_DETECTION_COOLDOWN * 1000LL))
+            {
+                timer_reset(CONFIG_INTERCOM_DEEP_SLEEP_DELAY);
+                door_sensor_timestamp = timestamp;
+                door_notification_pending = true;
+                ESP_LOGD(main_log_tag, "door_notification_pending set to true");
+            }
+        }
+
         if((event_bits & EVENT_RING_SENSOR_END) == EVENT_RING_SENSOR_END)
         {
             ESP_LOGD(main_log_tag, "Ring end detected!");
+        }
+
+        if((event_bits & EVENT_DOOR_SENSOR_END) == EVENT_DOOR_SENSOR_END)
+        {
+            ESP_LOGD(main_log_tag, "Door end detected!");
         }
 
         if(wifi_connected && ring_notification_pending)
@@ -273,6 +387,19 @@ extern "C" void app_main()
             
             // Clear pending flag, since we don't want deferred notification
             ring_notification_pending = false;
+        }
+
+        if(wifi_connected && door_notification_pending)
+        {
+            int64_t timestamp = esp_timer_get_time();
+            if(door_notification_timestamp == -1 || (timestamp - door_notification_timestamp > CONFIG_INTERCOM_RING_NOTIFICATION_COOLDOWN * 1000LL))
+            {
+                door_notification_timestamp = timestamp;
+                send_door_notification();
+            }
+            
+            // Clear pending flag, since we don't want deferred notification
+            door_notification_pending = false;
         }
 
 #if CONFIG_INTERCOM_BOOT_NOTIFICATION
